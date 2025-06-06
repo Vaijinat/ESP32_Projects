e453d51e11471aa71eb52450399b1ce5a9b3f43c
vaijinat <vaijinathmalge79@gmail.com>
2025-06-06 17:25:14 +0530

    Updated

---
+++ sw_led/sw_led.ino
@@ -0,0 +1,19 @@
+#define LED_PIN 15
+#define SWITCH_PIN 12
+bool ledState=false;
+bool prevSwitchState=HIGH;
+void setup()
+{
+  pinMode(LED_PIN,OUTPUT);
+  pinMode(SWITCH_PIN,INPUT_PULLUP);
+}
+void loop()
+{
+  bool currentSwitchState=digitalRead(SWITCH_PIN);
+  if(prevSwitchState==LOW&& currentSwitchState==HIGH)
+  {
+    ledState=!ledState;
+    digitalWrite(LED_PIN,ledState ? HIGH : LOW);
+  }
+  prevSwitchState=currentSwitchState;
+}
